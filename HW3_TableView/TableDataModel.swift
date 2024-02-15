5a58ef41de6670f6dcaf11a2f55e838af4d554fe
ShakirKadirov <shakirkadirov1@gmail.com>
2024-02-15 12:44:21 +0600

    tableView and TableViewModel

---
+++ HW3_TableView/TableDataModel.swift
@@ -0,0 +1,15 @@
+//
+//  TableDataModel.swift
+//  HW3_TableView
+//
+//  Created by Shakir Kadirov on 15.02.2024.
+//
+
+import Foundation
+
+
+struct TableDataModel{
+    var name: String
+    var lastName: String
+    var image: String
+}
--- HW3_TableView/ViewController.swift
+++ HW3_TableView/ViewController.swift
@@ -8,13 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let tableData: [TableDataModel] = []
+    
+    lazy var tableView: UITableView = {
+        $0.register(<#T##nib: UINib?##UINib?#>, forCellReuseIdentifier: <#T##String#>)
+        $0.backgroundColor = .white
+        $0.dataSource = self
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UITableView())
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemMint
+        setConstraints ()
     }
+    
+    private func setConstraints (){
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+    }
+}
 
 
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
 
